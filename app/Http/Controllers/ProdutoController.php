8e27932a6f62d7576bec7fe122ee0279adaae655
helena.franca <franca.helena333@gmail.com>
2026-05-26 16:56:00 -0300

    wip:produto_controller

--- app/Http/Controllers/ProdutoController.php
+++ app/Http/Controllers/ProdutoController.php
@@ -1,10 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Produto
 use Illuminate\Http\Request;
 
 class ProdutoController extends Controller
 {
-    //
+    public fumction inex(){
+        $produtos = produto::orderBy('nome')->get();
+        return View('produtos.index',compact('produtos'));
+    }
 }
--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model
 {
-    //
+    //define os compos em massa
+   protected $fillsble = ['nome', 'preco', 'estoque'];
 }
