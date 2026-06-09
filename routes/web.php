0a16bfb942006be6b8e71198a9b9237e4fbefabb
maria helena <franca.helena333@gmail.com>
2026-06-09 10:23:29 -0300

    wip_at_livros

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
 <?php
-use App\http\Controllers\ProdutoController;
+
+use App\Http\Controllers\ProdutoController;
+use App\Http\Controllers\LivroController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/produtos',[ProdutoController::class , 'index']);
-Route::post('/produtos',[ProdutoController::class , 'store']);
+
+Route::get('/produtos', [ProdutoController::class, 'index']);
+Route::post('/produtos', [ProdutoController::class, 'store']);
+
+Route::get('/livros', [LivroController::class, 'index']);
+Route::post('/livros', [LivroController::class, 'store']);
