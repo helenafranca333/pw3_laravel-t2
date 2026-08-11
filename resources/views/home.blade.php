c315213975a2027aa2fc5e3c5dcca8cff8a5fcd9
helena.franca <franca.helena333@gmail.com>
2026-08-11 13:28:59 -0300

    wip.aulafalite

---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@section('title','inicio')
+@section('content')
+   <section>
+        <h2>bem vindo ao projeto</h2>
+        <p>essa tela reutiliza o layout padrao</p>
+   </section>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@ use App\http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
 Route::get('/produtos',[ProdutoController::class , 'index']);
 Route::post('/produtos',[ProdutoController::class , 'store']);
