851f263243b7077d2e0ab6c8d66075c5093c9069
helena.franca <franca.helena333@gmail.com>
2026-08-17 14:37:47 -0300

    wip:admintailnd

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'projeto pw3')</title>
     <link rel="stylesheet" href="{{asset('assets/css/app.css')}}">
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 
 <body>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+Route::View('/landing','landing');
+Route::View('/admin','admin.dashboard');
+
 Route::get('/produtos',[ProdutoController::class , 'index']);
 Route::post('/produtos',[ProdutoController::class , 'store']);
-Route::View('/landing','landing');
